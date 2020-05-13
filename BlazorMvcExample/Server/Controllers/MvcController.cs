83ebe07a20d6ca7280306613cf77b0881bfacbff
Howard Richards <dev@conficient.com>
2020-05-13 10:44:07 +0100

    Added Mvc controller and links to the routes in the home page

---
+++ BlazorMvcExample/Server/Controllers/MvcController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMvcExample.Server.Controllers
+{
+    public class MvcController : Controller
+    {
+        /// <summary>
+        /// This method is public, confirms routing
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("mvc/public")] public IActionResult Public() => Content("<h1>This view is public</h1>");
+        
+        [Authorize, HttpGet("mvc/private")] public IActionResult Private() => Content("<h1>This view is private</h1>");
+    }
+}
